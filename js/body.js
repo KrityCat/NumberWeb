107cefcac13ecda25bd6a15394c5169852662d81
KrityCat <508441878@qq.com>
2022-02-11 22:50:09 +0800

    three

--- js/body.js
+++ js/body.js
@@ -14,3 +14,15 @@ function NumberTest() {
 function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
+
+function test(str){
+    switch(str){
+        case "alert-success": success_prompt("提交成功");break;
+        case "alert_warning": warning_prompt("错误警告");break;
+        case "alert-danger": fail_prompt("提交失败");break;
+        case "alert_info":	info_prompt("未查询到数据");break;
+        default:alert_prompt("未查询到数据");
+    }
+    
+}
+
